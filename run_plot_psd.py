3910545034ba16889dfa1483c026613e16ea173f
seiscode <muleiyu@outlook.com>
2025-08-26 12:14:57 +0800

    Update run_plot_psd.py

--- run_plot_psd.py
+++ run_plot_psd.py
@@ -4,11 +4,11 @@ import os
 import sys
 
 """
-:Author:
+Author:
     muly (muly@cea-igp.ac.cn)
-:license:
-    GNU Lesser General Public License, Version 3
-    (https://www.gnu.org/copyleft/lesser.html)
+license:
+    MIT License
+    (https://opensource.org/licenses/MIT)
 """
 """
 PPSD绘图脚本
