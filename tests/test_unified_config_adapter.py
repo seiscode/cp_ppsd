939397eee3e368d19554e6e91451d5467be71943
seiscode <muleiyu@outlook.com>
2025-08-26 12:11:46 +0800

    Update test_unified_config_adapter.py

--- tests/test_unified_config_adapter.py
+++ tests/test_unified_config_adapter.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-Author: 
+Author:
     muly (muly@cea-igp.ac.cn)
-:license:
-    GNU Lesser General Public License, Version 3
-    (https://www.gnu.org/copyleft/lesser.html)
+license:
+    MIT License
+    (https://opensource.org/licenses/MIT)
 
 统一配置适配器测试和演示脚本
 
