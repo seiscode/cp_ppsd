1b251e9a6959d071503a399c101f8544caca096c
seiscode <muleiyu@outlook.com>
2025-08-26 10:59:59 +0800

    Update cp_psd.py

--- cp_ppsd/cp_psd.py
+++ cp_ppsd/cp_psd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-:Author:
+Author:
     muly (muly@cea-igp.ac.cn)
-:license:
-    GNU Lesser General Public License, Version 3
-    (https://www.gnu.org/copyleft/lesser.html)
+license:
+    MIT License
+    (https://opensource.org/licenses/MIT)
 """
 """
 PPSD 批量处理与可视化工具
