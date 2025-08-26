e1b33dffa4728024e475ab0ea951512349aab7d3
seiscode <muleiyu@outlook.com>
2025-08-26 12:14:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ CP-PPSD 安装脚本
 """
 Author:
     muly (muly@cea-igp.ac.cn)
-:license:
-    GNU Lesser General Public License, Version 3
-    (https://www.gnu.org/copyleft/lesser.html)
+license:
+    MIT License
+    (https://opensource.org/licenses/MIT)
 """
 
 
